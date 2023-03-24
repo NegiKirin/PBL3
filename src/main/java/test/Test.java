3040f1700b753b6d262647262d2cb5b6a4887b12
NegiKirin <phuc12345ho@gmail.com>
2023-03-24 19:00:17 +0700

    fix

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -17,12 +17,12 @@ public class Test {
 		School s2 = new School("Nguyen Thuong Hien");
 		School s3 = new School("Nguyen Van Troi");
 		
-		User u1 = new User("SWM1", "abc@gmail.com", "123123123", "Ho", "Phuc", new Date(100, 4, 30), true,new Date(System.currentTimeMillis()),s2);
-		User u2 = new User("SWM2", "abc@gmail.com", "abcdefgh", "Le", "Thanh", new Date(99, 3, 20), true,new Date(System.currentTimeMillis()),s1);
-		User u3 = new User("SWM3", "abc@gmail.com", "qwezxcv", "Nguyen", "Tan", new Date(102, 7, 1), true,new Date(System.currentTimeMillis()),s3);
-		User u4 = new User("SWM4", "abc@gmail.com", "qweqwe", "Leo", "Messi", new Date(103, 2, 27), true,new Date(System.currentTimeMillis()),s1);
-		User u5 = new User("SWM5", "abc@gmail.com", "zxczxczxc", "Nguyen", "Van A", new Date(105, 6, 30), true,new Date(System.currentTimeMillis()),s2);
-		
+//		User u1 = new User("SWM1", "abc@gmail.com", "123123123", "Ho", "Phuc", new Date(100, 4, 30), true,new Date(System.currentTimeMillis()),s2);
+//		User u2 = new User("SWM2", "abc@gmail.com", "abcdefgh", "Le", "Thanh", new Date(99, 3, 20), true,new Date(System.currentTimeMillis()),s1);
+//		User u3 = new User("SWM3", "abc@gmail.com", "qwezxcv", "Nguyen", "Tan", new Date(102, 7, 1), true,new Date(System.currentTimeMillis()),s3);
+//		User u4 = new User("SWM4", "abc@gmail.com", "qweqwe", "Leo", "Messi", new Date(103, 2, 27), true,new Date(System.currentTimeMillis()),s1);
+//		User u5 = new User("SWM5", "abc@gmail.com", "zxczxczxc", "Nguyen", "Van A", new Date(105, 6, 30), true,new Date(System.currentTimeMillis()),s2);
+//		
 		
 		Appointment a1 = new Appointment("001", new Time(7, 30, 0), new Time(8, 30, 0), "73 Nguyen Luong Bang", 10);
 		Appointment a2 = new Appointment("002", new Time(13, 30, 0), new Time(17, 30, 0), "123 Tran Cao Van", 7);
@@ -33,12 +33,12 @@ public class Test {
 		SchoolDAO schoolDAO = new SchoolDAO();
 		AppointmentDAO appointmentDAO = new AppointmentDAO();
 		
-		userDAO.insert(u1);
-		userDAO.insert(u2);
-		userDAO.insert(u3);
-		userDAO.insert(u4);
-		userDAO.insert(u5);
-		
+//		userDAO.insert(u1);
+//		userDAO.insert(u2);
+//		userDAO.insert(u3);
+//		userDAO.insert(u4);
+//		userDAO.insert(u5);
+//		
 		
 	}
 }
