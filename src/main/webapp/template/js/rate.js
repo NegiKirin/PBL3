5a2364e3754115c38efce9c935d72ba30a5452f4
PhucTanNP <126167403+PhucTanNP@users.noreply.github.com>
2023-05-24 17:01:08 +0700

    tan

---
+++ src/main/webapp/template/js/rate.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function() {
+    $('.show-button').click(function() {
+        // $('.list').css('display', 'inline-block');
+        $(this).parent().next('.list').slideToggle();
+        $(this).find('.arrow').toggleClass('rotate');
+    });
+});x
+
